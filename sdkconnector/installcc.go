8773441e1be326b68f55acd871d63a99e7c58f49
Basil K Y <techiebasil@gmail.com>
2020-02-02 12:10:11 +0530

    installcc function

---
+++ sdkconnector/installcc.go
@@ -0,0 +1,34 @@
+package sdkconnector
+
+import (
+	"os"
+
+	"github.com/hyperledger/fabric-sdk-go/pkg/client/resmgmt"
+	"github.com/hyperledger/fabric-sdk-go/pkg/common/errors/retry"
+	packager "github.com/hyperledger/fabric-sdk-go/pkg/fab/ccpackager/gopackager"
+	"github.com/hyperledger/fabric-sdk-go/pkg/fabsdk"
+)
+
+func InstallCC(orgname string, username string, chaincodepath string, chaincodename string, chaincodeversion string) error {
+
+	sdk, err := CreateSDKInstance(orgname)
+	if err != nil {
+		return err
+	}
+	resourceManagerClientContext := sdk.Context(fabsdk.WithUser(username), fabsdk.WithOrg(orgname))
+	resMgmtClient, err := resmgmt.New(resourceManagerClientContext)
+	if err != nil {
+		return err
+	}
+	ccPkg, err := packager.NewCCPackage(chaincodepath, os.Getenv("GOPATH"))
+	if err != nil {
+		return err
+	}
+	installCCReq := resmgmt.InstallCCRequest{Name: chaincodename, Path: chaincodepath, Version: chaincodeversion, Package: ccPkg}
+	_, err = resMgmtClient.InstallCC(installCCReq, resmgmt.WithRetry(retry.DefaultResMgmtOpts))
+	if err != nil {
+		return err
+	}
+	sdk.Close()
+	return nil
+}
